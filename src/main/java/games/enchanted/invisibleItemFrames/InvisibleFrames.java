f7cb409868af80e4ebba9074a1ec5f90fa086f0c
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-05-22 18:22:15 +0100

    upgrade to gradle 8.6 and fabric loom 1.6

--- src/main/java/games/enchanted/invisibleItemFrames/InvisibleFrames.java
+++ src/main/java/games/enchanted/invisibleItemFrames/InvisibleFrames.java
@@ -10,12 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class InvisibleFrames implements ModInitializer {
-	public static final String MOD_ID = "eg-invisible-frames";
-	public static final Logger log = LoggerFactory.getLogger( MOD_ID );
-	
+  public static final String MOD_ID = "eg-invisible-frames";
+  public static final Logger log = LoggerFactory.getLogger( MOD_ID );
+
   public static final TagKey<Item> MAKES_ITEM_FRAMES_INVISIBLE_TAG = TagKey.of( RegistryKeys.ITEM, new Identifier( MOD_ID, "makes_item_frames_invisible" ) );
 
-	@Override
-	public void onInitialize() {
-	}
+  @Override
+  public void onInitialize() {
+  }
 }
