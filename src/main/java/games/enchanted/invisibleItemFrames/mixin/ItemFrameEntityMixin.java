709a7c885f5a98ae15c8829b82f8bdbe0f0de765
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-02-22 21:28:59 +0000

    update to 1.21.4 and hopefully improve mixin compatibility

--- src/main/java/games/enchanted/invisibleItemFrames/mixin/ItemFrameEntityMixin.java
+++ src/main/java/games/enchanted/invisibleItemFrames/mixin/ItemFrameEntityMixin.java
@@ -1,25 +1,18 @@
 package games.enchanted.invisibleItemFrames.mixin;
 
+import games.enchanted.invisibleItemFrames.duck.InvisibleFramesAccess;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.decoration.AbstractDecorationEntity;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 
@@ -27,9 +20,6 @@ import games.enchanted.invisibleItemFrames.InvisibleFrames;
 
 import java.util.ArrayList;
 
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -39,37 +29,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemFrameEntity.class)
-public class ItemFrameEntityMixin extends AbstractDecorationEntity {
+public abstract class ItemFrameEntityMixin extends AbstractDecorationEntity implements InvisibleFramesAccess {
 	public ItemFrameEntityMixin(EntityType<? extends AbstractDecorationEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
 	@Shadow
 	private boolean fixed;
-	@Unique
-	private ItemStack GLASS_PANE_ITEM = ItemStack.EMPTY;
-	@Unique
-	private ArrayList<ServerPlayerEntity> playersTrackingThisFrame = new ArrayList<>();
+	@Shadow
+	public void onPlace() {
+		throw new AssertionError( "onPlace not shadowed" );
+	};
+	@Shadow
+	public ItemStack getHeldItemStack() {
+		throw new AssertionError( "getHeldItemStack not shadowed" );
+	};
 
 	@Unique
-	@Override
-	public void initDataTracker(DataTracker.Builder builder) {
-	}
-
+	private ItemStack invisible_frames$GLASS_PANE_ITEM = ItemStack.EMPTY;
 	@Unique
-	@Override
-	public Box calculateBoundingBox(BlockPos pos, Direction side) {
-		return null;
-	}
+	private ArrayList<ServerPlayerEntity> invisible_frames$playersTrackingThisFrame = new ArrayList<>();
 
 	// check if a player attacks ItemFrame while holding any item from #eg-invisible-frames:makes_item_frames_invisible
 	//   if so, set ItemFrame to invisible and save the glass pane
 	@Inject(
-		at = @At( value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;getHeldItemStack()Lnet/minecraft/item/ItemStack;", shift = At.Shift.AFTER ),
-		method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z",
+		at = @At( value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;isAlwaysInvulnerableTo(Lnet/minecraft/entity/damage/DamageSource;)Z", shift = At.Shift.AFTER ),
+		method = "damage",
 		cancellable = true
 	)
-	public void damage( DamageSource source, float amount, CallbackInfoReturnable<Boolean> ci ) {
+	public void damage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir ) {
 		Entity attacker = source.getAttacker();
 
 		// if player damages an item frame that isn't invisible
@@ -87,28 +75,28 @@ public class ItemFrameEntityMixin extends AbstractDecorationEntity {
 				this.setInvisible( true );
 				this.playSound( SoundEvents.ENTITY_ITEM_FRAME_PLACE, 1.0F, 1.0F );
 				
-				ci.setReturnValue( true );
+				cir.setReturnValue( true );
 			}
 		}
 		// if player damages an item frame that is invisible, has a glass pane item and has no held item
 		else if ( attacker instanceof PlayerEntity && this.isInvisible() && !this.getGlassPaneItemStack().isEmpty() && this.getHeldItemStack().isEmpty() ) {
 			this.dropGlassPaneStack( attacker );
 			this.setInvisible( false );
-			ci.setReturnValue( true );
+			cir.setReturnValue( true );
 		}
 	}
 
 	// drop glass pane when item frame breaks
-	@Inject(at = @At("HEAD"), method = "onBreak(Lnet/minecraft/entity/Entity;)V")
-	private void onBreak( @Nullable Entity entity, CallbackInfo ci ) {
-		this.dropGlassPaneStack( entity );
+	@Inject(at = @At("HEAD"), method = "onBreak")
+	private void onBreak(ServerWorld world, Entity breaker, CallbackInfo ci ) {
+		this.dropGlassPaneStack( breaker );
 	}
 	
 	// save glass_pane_item to ItemFrame NBT
 	@Inject(at = @At("HEAD"), method = "writeCustomDataToNbt(Lnet/minecraft/nbt/NbtCompound;)V")
 	private void writeCustomDataToNbt( NbtCompound nbt, CallbackInfo ci ) {
 		if( !getGlassPaneItemStack().isEmpty() ) {
-			nbt.put( "glass_pane_item", getGlassPaneItemStack().encode( this.getRegistryManager() ) );
+			nbt.put( "glass_pane_item", getGlassPaneItemStack().toNbt( this.getRegistryManager() ) );
 		}
 	}
 	
@@ -124,33 +112,11 @@ public class ItemFrameEntityMixin extends AbstractDecorationEntity {
 		}
 		this.setGlassPaneItemStack( itemStack );
 	}
-
-	@Override
-	public void tick() {
-		super.tick();
-		// summon a particle if ItemFrame is invisible, has a glass_pane_item in its NBT, is empty, and is less than 15 blocks away from player
-		if( !getGlassPaneItemStack().isEmpty() && this.isInvisible() && getHeldItemStack().isEmpty() && random.nextInt(30) == 0) {
-			Box boundingBox = this.getBoundingBox();
-
-			double x = boundingBox.minX + (boundingBox.getLengthX() * random.nextDouble());
-			double y = boundingBox.minY + (boundingBox.getLengthY() * random.nextDouble());
-			double z = boundingBox.minZ + (boundingBox.getLengthZ() * random.nextDouble());
-			if(this.facing == Direction.UP) {
-				y += 0.125;
-			}
-
-			for ( ServerPlayerEntity player : playersTrackingThisFrame ) {
-				if(player.distanceTo(this) > 15 && player.canSee(this)) {
-					return;
-				}
-				player.networkHandler.sendPacket( new ParticleS2CPacket( ParticleTypes.END_ROD, true, x, y, z, 0.0f, 0.0f, 0.0f, 0.0f, 1 ) );
-			}
-		}
-	}
 	
 	@Unique
 	private void dropGlassPaneStack( Entity entity ) {
-		if ( this.fixed || !this.getWorld().getGameRules().getBoolean( GameRules.DO_ENTITY_DROPS ) ) {
+		if(!(this.getWorld() instanceof ServerWorld serverWorld)) return;
+		if ( this.fixed || !serverWorld.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS) ) {
 			return;
 		}
 		
@@ -163,38 +129,35 @@ public class ItemFrameEntityMixin extends AbstractDecorationEntity {
 			}
 		}
 		
-		this.dropStack( paneStack );
+		this.dropStack( serverWorld, paneStack );
 		setGlassPaneItemStack( ItemStack.EMPTY );
 	}
 
 	@Unique
 	private ItemStack getGlassPaneItemStack() {
-		return GLASS_PANE_ITEM;
+		return invisible_frames$GLASS_PANE_ITEM;
 	}
 
 	@Unique
 	private void setGlassPaneItemStack( ItemStack stack ) {
-		GLASS_PANE_ITEM = stack;
+		invisible_frames$GLASS_PANE_ITEM = stack;
 	}
 	
 	public void onStartedTrackingBy( ServerPlayerEntity player ) {
-		playersTrackingThisFrame.add( player );
+		invisible_frames$playersTrackingThisFrame.add( player );
 	}
 	
 	public void onStoppedTrackingBy( ServerPlayerEntity player ){
-		playersTrackingThisFrame.remove( player );
+		invisible_frames$playersTrackingThisFrame.remove( player );
 	}
 
-	@Shadow
-	public void onPlace() {
-		throw new AssertionError( "onPlace not shadowed" );
-	};
-	@Shadow
-	public void onBreak(Entity arg0) {
-		throw new AssertionError( "onBreak not shadowed" );
-	};
-	@Shadow
-	public ItemStack getHeldItemStack() {
-		throw new AssertionError( "getHeldItemStack not shadowed" );
-	};
+	@Override
+	public ItemStack invisible_frames$getGlassPaneStack() {
+		return this.getGlassPaneItemStack();
+	}
+
+	@Override
+	public ArrayList<ServerPlayerEntity> invisible_frames$getTrackedPlayers() {
+		return this.invisible_frames$playersTrackingThisFrame;
+	}
 }
