dd89f3ed46861b8bb2937da9a254036d359d270f
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-04-11 00:26:33 +0100

    fix fade out animation rotation

--- common/src/main/java/games/enchanted/invisibleframes/ItemFrameGhostManager.java
+++ common/src/main/java/games/enchanted/invisibleframes/ItemFrameGhostManager.java
@@ -1,8 +1,10 @@
 package games.enchanted.invisibleframes;
 
+import com.mojang.math.Axis;
 import com.mojang.math.Transformation;
 import games.enchanted.invisibleframes.mixin.access.DisplayAccess;
 import games.enchanted.invisibleframes.mixin.access.TextDisplayAccessor;
+import net.minecraft.core.Direction;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
@@ -53,24 +55,43 @@ public class ItemFrameGhostManager {
         if(display != null) {
             removeEntities();
         }
+        Direction direction = itemFrame.getDirection();
+
+        // create text display and set position
         display = new Display.TextDisplay(EntityType.TEXT_DISPLAY, this.level);
         display.setPos(
-            itemFrame.getX() + (-0.0312f * itemFrame.getDirection().getStepX()),
-            itemFrame.getY() + (-0.0312f * itemFrame.getDirection().getStepY()),
-            itemFrame.getZ() + (-0.0312f * itemFrame.getDirection().getStepZ())
+            itemFrame.getX() + (-0.03f * direction.getStepX()),
+            itemFrame.getY() + (-0.03f * direction.getStepY()),
+            itemFrame.getZ() + (-0.03f * direction.getStepZ())
         );
 
-        ((TextDisplayAccessor) display).invisibleFrames$setText(Component.literal(" "));
-        ((DisplayAccess) display).invisibleFrames$setInterpolationDuration(FADE_OUT_TICKS);
-        ((DisplayAccess) display).invisibleFrames$setInterpolationDelay(1);
-        ((TextDisplayAccessor) display).invisibleFrames$setBackgroundColor(0xffffffff);
+        // rotate text display to match the item frame
+        float xRot;
+        float yRot;
+        if (direction.getAxis().isHorizontal()) {
+            xRot = 0.0f;
+            yRot = 180.0f - direction.getOpposite().toYRot();
+        } else {
+            xRot = 90 * direction.getAxisDirection().getStep();
+            yRot = 180.0f;
+        }
+        Quaternionf rotationQuaternion = Axis.XP.rotationDegrees(xRot);
+        rotationQuaternion.mul(Axis.YP.rotationDegrees(yRot));
+
         ((DisplayAccess) display).invisibleFrames$setTransformation(new Transformation(
-            new Vector3f(-0.075f, 0f, 0.375f),
-            new Quaternionf(-0.707f, 0f, 0f, 0.707f),
+            new Vector3f(-0.075f, -0.37f, 0f).rotate(rotationQuaternion),
+            rotationQuaternion,
             new Vector3f(6, 3, 6),
             new Quaternionf(0f, 0f, 0f, 1f)
         ));
 
+        // setup text display visuals
+        ((TextDisplayAccessor) display).invisibleFrames$setText(Component.literal(" "));
+        ((DisplayAccess) display).invisibleFrames$setInterpolationDuration(FADE_OUT_TICKS);
+        ((DisplayAccess) display).invisibleFrames$setInterpolationDelay(1);
+        ((TextDisplayAccessor) display).invisibleFrames$setBackgroundColor(0xffffffff);
+
+        // add data to prevent saving the text display entity
         CompoundTag compoundTag = new CompoundTag();
         compoundTag.putBoolean(InvisibleFramesConstants.ENTITY_NO_SAVE_TAG, true);
         display.setComponent(DataComponents.CUSTOM_DATA, CustomData.of(compoundTag));
