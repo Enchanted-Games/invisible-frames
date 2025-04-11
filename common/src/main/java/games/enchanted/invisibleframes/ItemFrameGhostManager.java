bbca684dc9a2f0a5b26972a287b0fddb4bd51d73
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-04-11 14:32:47 +0100

    move particle spawning logic outside of BlockAttatchedEntityMixin

--- common/src/main/java/games/enchanted/invisibleframes/ItemFrameGhostManager.java
+++ common/src/main/java/games/enchanted/invisibleframes/ItemFrameGhostManager.java
@@ -2,17 +2,24 @@ package games.enchanted.invisibleframes;
 
 import com.mojang.math.Axis;
 import com.mojang.math.Transformation;
+import games.enchanted.invisibleframes.duck.InvisibleFramesAccess;
 import games.enchanted.invisibleframes.mixin.access.DisplayAccess;
+import games.enchanted.invisibleframes.mixin.access.HangingEntityAccess;
 import games.enchanted.invisibleframes.mixin.access.TextDisplayAccessor;
 import net.minecraft.core.Direction;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.phys.AABB;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
@@ -37,8 +44,14 @@ public class ItemFrameGhostManager {
     }
 
     public void tick() {
+        // summon a particle if ItemFrame is invisible, has an item it was made invisible by, is empty, and is less than 15 blocks away from player
+        if(timeAlive <= 1 && !((InvisibleFramesAccess) itemFrame).invisibleFrames$getInvisibleItem().isEmpty() && itemFrame.isInvisible() && itemFrame.getItem().isEmpty() && itemFrame.getRandom().nextInt(30) == 0) {
+            spawnRandomParticle();
+        }
         if(display == null) return;
+
         timeAlive++;
+
         if(timeAlive == 1) {
             ((TextDisplayAccessor) display).invisibleFrames$setBackgroundColor(ENDING_COLOUR);
         }
@@ -101,5 +114,28 @@ public class ItemFrameGhostManager {
         display.setComponent(DataComponents.CUSTOM_DATA, CustomData.of(compoundTag));
 
         this.level.addFreshEntity(display);
+    }
+
+    public void spawnRandomParticle() {
+        RandomSource random = itemFrame.getRandom();
+        AABB boundingBox = itemFrame.getBoundingBox();
+        Direction frameDirection = ((HangingEntityAccess) itemFrame).invisible_frames$getDirection();
+
+        double x = random.nextFloat() * boundingBox.getXsize();
+        double y = random.nextFloat() * boundingBox.getYsize();
+        double z = random.nextFloat() * boundingBox.getZsize();
+
+        x += boundingBox.minX;
+        y += boundingBox.minY;
+        z += boundingBox.minZ;
+
+        if(frameDirection != Direction.DOWN) {
+            y += 0.125;
+        }
+
+        for (ServerPlayer player : ((InvisibleFramesAccess) itemFrame).invisibleFrames$getTrackedPlayers()) {
+            if(player.distanceTo(itemFrame) > 15 && player.hasLineOfSight(itemFrame)) return;
+            player.connection.send(new ClientboundLevelParticlesPacket(ParticleTypes.END_ROD, false, true, x, y, z, 0.0f, 0.0f, 0.0f, 0.0f, 1));
+        }
     }
 }
--- common/src/main/java/games/enchanted/invisibleframes/mixin/BlockAttachedEntityMixin.java
+++ common/src/main/java/games/enchanted/invisibleframes/mixin/BlockAttachedEntityMixin.java
@@ -1,17 +1,11 @@
 package games.enchanted.invisibleframes.mixin;
 
 import games.enchanted.invisibleframes.duck.InvisibleFramesAccess;
-import games.enchanted.invisibleframes.mixin.access.HangingEntityAccess;
-import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.decoration.BlockAttachedEntity;
 import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -30,21 +24,5 @@ public abstract class BlockAttachedEntityMixin extends Entity {
     public void tick(CallbackInfo ci) {
         if(!((Object) this instanceof ItemFrame itemFrameEntity)) return;
         ((InvisibleFramesAccess) itemFrameEntity).invisibleFrames$tickGhostManager();
-        // summon a particle if ItemFrame is invisible, has an item it was made invisible by, is empty, and is less than 15 blocks away from player
-        if(!((InvisibleFramesAccess) itemFrameEntity).invisibleFrames$getInvisibleItem().isEmpty() && this.isInvisible() && itemFrameEntity.getItem().isEmpty() && random.nextInt(30) == 0) {
-            AABB boundingBox = this.getBoundingBox();
-
-            double x = boundingBox.minX + (boundingBox.getXsize() * random.nextDouble());
-            double y = boundingBox.minY + (boundingBox.getYsize() * random.nextDouble());
-            double z = boundingBox.minZ + (boundingBox.getZsize() * random.nextDouble());
-            if(((HangingEntityAccess) itemFrameEntity).invisible_frames$getDirection() == Direction.UP) {
-                y += 0.125;
-            }
-
-            for (ServerPlayer player : ((InvisibleFramesAccess) itemFrameEntity).invisibleFrames$getTrackedPlayers()) {
-                if(player.distanceTo(this) > 15 && player.hasLineOfSight(this)) return;
-                player.connection.send( new ClientboundLevelParticlesPacket( ParticleTypes.END_ROD, false, true, x, y, z, 0.0f, 0.0f, 0.0f, 0.0f, 1 ) );
-            }
-        }
     }
 }
